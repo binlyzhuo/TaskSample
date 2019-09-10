b3a4738be17212c249c9daecb108fd6fc544eed4
binlyz <binlyzhuo@outlook.com>
2019-09-10 10:05:44 +0800

    msg

--- TaskSampleConsole/Program.cs
+++ TaskSampleConsole/Program.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
 //https://www.cnblogs.com/wyy1234/p/9172467.html
+//https://www.cnblogs.com/wyy1234/p/9172467.html
+
 
 namespace TaskSampleConsole
 {
@@ -14,34 +17,42 @@ namespace TaskSampleConsole
         static void Main(string[] args)
         {
             Console.WriteLine("主线程执行业务处理");
-            Task task = new Task(() =>
-            {
-                Console.WriteLine("使用Task执行异步操作");
-                for (int i = 0; i < 10; i++)
-                {
-                    Console.WriteLine(i);
-                }
-            });
+            //Task task = new Task(() =>
+            //{
+            //    Console.WriteLine("使用Task执行异步操作");
+            //    for (int i = 0; i < 10; i++)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //});
 
-            task.Start();
-            Console.WriteLine("主线程执行其他处理");
-            //Console.ReadLine();
-            Thread.Sleep(2000);
+            //task.Start();
+            //Console.WriteLine("主线程执行其他处理");
+            ////Console.ReadLine();
+            //Thread.Sleep(2000);
 
-            //TaskWait();
+            ////TaskWait();
 
-            TaskContinueWith();
+            //TaskContinueWith();
 
-            Console.WriteLine("线程池操作");
+            //Console.WriteLine("线程池操作");
 
-            for (int i = 0; i < 10; i++)
-            {
-                ThreadPool.QueueUserWorkItem(new WaitCallback((obj) =>
-                {
-                    Console.WriteLine($"第{obj}个任务");
-                }),i);
-            }
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    ThreadPool.QueueUserWorkItem(new WaitCallback((obj) =>
+            //    {
+            //        Console.WriteLine($"第{obj}个任务");
+            //    }),i);
+            //}
 
+
+            //TaskCancel();
+
+            //string content = GetContentAsync("txt.txt").Result;
+            //Console.WriteLine(content);
+
+            string content2 = GetContent("txt.txt");
+            Console.WriteLine(content2);
             Console.ReadLine();
 
         }
@@ -93,6 +104,57 @@ namespace TaskSampleConsole
             });
 
 
+        }
+
+
+        static void TaskCancel()
+        {
+            Console.WriteLine("Task Cancel...");
+            CancellationTokenSource source = new CancellationTokenSource();
+            
+            int index = 0;
+            Task task1 = new Task(() =>
+            {
+                while (!source.IsCancellationRequested)
+                {
+                    Thread.Sleep(1000);
+                    Console.WriteLine($"第{++index}次执行，线程运行中...");
+                }
+            });
+
+            task1.Start();
+            Thread.Sleep(5000);
+            source.Token.Register(() =>
+            {
+                Console.WriteLine("任务已取消。。");
+            });
+            source.Cancel();
+            Console.WriteLine("任务已取消");
+        }
+
+        static async Task<string> GetContentAsync(string fileName)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Open);
+            var bytes = new byte[fs.Length];
+            Console.WriteLine("开始读取文件....");
+            int len = await fs.ReadAsync(bytes, 0, bytes.Length);
+            string result = Encoding.UTF8.GetString(bytes);
+            return result;
+        }
+
+        static string GetContent(string fileName)
+        {
+            object locker = new object();
+
+            lock (locker)
+            {
+                FileStream fs = new FileStream(fileName, FileMode.Open);
+                var bytes = new byte[fs.Length];
+                int len = fs.Read(bytes, 0, bytes.Length);
+                string result = Encoding.UTF8.GetString(bytes);
+                return result;
+            }
+            
         }
     }
 }
